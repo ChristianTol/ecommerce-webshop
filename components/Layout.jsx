9cc32c38fdf0dc5508caf3a5643cd8f0e76222ed
ChristianTol <christian.tol1998@hotmail.com>
2022-11-23 12:28:01 +0100

    NavBar and footer added.

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -5,7 +5,7 @@ import Footer from "./Footer";
 
 const Layout = ({ children }) => {
   return (
-    <div>
+    <div className="layout">
       <Head>
         <title>Electronics Store</title>
       </Head>
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,9 +1,20 @@
 import React from "react";
+import Link from "next/link";
+import { AiOutlineShopping } from "react-icons/ai";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <div className="navbar-container">
+      <p>
+        <Link href="/">Electronics Store</Link>
+      </p>
+
+      <button type="button" className="cart-icon" onClick="">
+        <AiOutlineShopping />
+        <span className="cart-item-qty">1</span>
+      </button>
+    </div>
+  );
+};
 
 export default Navbar;
